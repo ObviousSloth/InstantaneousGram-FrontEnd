29725e76ddf2bd4b9d7fde17ea5228ae8b922943
ObviousSloth <68105844+ObviousSloth@users.noreply.github.com>
2024-03-21 00:04:19 +0100

    Set up Auth0 and base react
    Added auth0 login, logout and profile info gathering. also added basic pages and router

--- my-app/src/Pages/HomePage.js
+++ my-app/src/Pages/HomePage.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../components/Navbar.js';
+import LoginButton from '../components/Login.js';
+import LogoutButton from '../components/LogOut.js';
+import UserProfileButton from '../components/UserProfile.js';
 
-function HomePage() {
-    return (
-        <div>
-            <h1>Welcome to InstantaneousGram!</h1>
-            <p>Share your moments with the world.</p>
-            {/* Add your content here */}
-        </div>
-    );
+function Home() {
+  return (
+    
+    <div className="container mt-5">
+        
+      <h1 className="display-4">Welcome to My Website</h1>
+      <Navbar />
+      <LoginButton />
+      <br />
+      <LogoutButton />
+      <br />
+      <UserProfileButton />
+      <p className="lead">This is a simple homepage built with React.</p>
+      <p className="lead">Feel free to explore!</p>
+      <Link to="/about" className="btn btn-primary">About Us</Link>
+      
+    </div>
+  );
 }
 
-export default HomePage;
+export default Home;
