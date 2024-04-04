2faaf72a4a7b4170db3c20a1542403f8c3929c4a
ObviousSloth <429428@student.fontys.nl>
2024-04-04 14:57:14 +0200

    Basic auth0 Setup

--- InstantaneousGram/src/index.js
+++ InstantaneousGram/src/index.js
@@ -7,7 +7,15 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+        <Auth0Provider
+      domain= {process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
